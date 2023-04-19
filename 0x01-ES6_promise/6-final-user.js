8b0ca6707def2f514be2a9ab190ae7b8b8d32bb9
seyistry <seyistry@gmail.com>
2023-04-19 20:03:04 +0100

    fix now return error code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const photo = uploadPhoto(fileName);
-  const user = signUpUser(firstName, lastName);
-  return Promise.all([user, photo]);
+  const myPromise = Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((values) => values)
+    .catch((error) => error.message);
+  return myPromise;
 }
