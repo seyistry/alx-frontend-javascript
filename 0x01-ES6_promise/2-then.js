7fd7416402430184e84c45f68dc85e084c543b2d
seyistry <seyistry@gmail.com>
2023-04-20 11:30:34 +0100

    return empty error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,7 +4,7 @@ export default function handleResponseFromAPI(promise) {
       status: 200,
       body: 'success',
     }))
-    .catch((err) => new Error(err))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
   return myPromise;
 }
