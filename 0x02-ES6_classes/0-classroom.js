d092a0215dbdaa4d90e5aa5167794eabed3f342a
seyistry <seyistry@gmail.com>
2023-04-20 19:28:08 +0100

    fix dangling error

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
 export default class ClassRoom {
-  _maxStudentsSize;
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+    this.maxStudentsSize = maxStudentsSize;
   }
 }
