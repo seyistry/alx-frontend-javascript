0318aef97394ec5779cfcb1fa6570eb8bdd84230
seyistry <seyistry@gmail.com>
2023-04-21 16:56:26 +0100

    fix code to print all error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
 
-    this._length = length;
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
 
     if (typeof students === 'object') {
       this._students = students;
