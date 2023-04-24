2dd531fea756a19bdc6bb7e5d2318a3e28b4ef78
seyistry <seyistry@gmail.com>
2023-04-24 18:11:12 +0100

    return empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (startString.length === 0) return '';
   const word = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
