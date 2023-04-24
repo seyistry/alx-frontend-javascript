ec70c03d13ac770353ae443f340dd742f8ee49de
seyistry <seyistry@gmail.com>
2023-04-24 18:32:06 +0100

    throw new error if request higher than 4

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(obj) {
   if (weakMap.has(obj)) {
-    if (num >= 5) throw Error('Endpoint load is high');
+    if (num >= 5) throw new Error('Endpoint load is high');
     weakMap.set(obj, (num += 1));
   } else {
     num = 1;
