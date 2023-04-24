ad8ebc1647fac40e3c06adca05b5e8849061994f
seyistry <seyistry@gmail.com>
2023-04-24 18:17:50 +0100

    check obj type

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(myMap) {
+  if (!(myMap instanceof Map)) throw Error('Cannot process');
   for (const [key, value] of myMap) {
     if (value === 1) {
       myMap.set(key, 100);
