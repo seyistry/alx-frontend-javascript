2f3a2e75262591da1054d8f32350161321c73e91
seyistry <seyistry@gmail.com>
2023-04-27 22:18:38 +0100

    throw error

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,15 +1,15 @@
 import { queryAPI, weakMap } from './100-weak.js';
 
 const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
+// weakMap.get(endpoint);
 
-queryAPI(endpoint);
+queryAPI(endpoint); // 1
 console.log(weakMap.get(endpoint));
 
-queryAPI(endpoint);
+queryAPI(endpoint); // 2
 console.log(weakMap.get(endpoint));
 
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+queryAPI(endpoint); // 3
+queryAPI(endpoint); // 4
+queryAPI(endpoint); // 5
+// queryAPI(endpoint);
--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
-let num = 0;
+let num = 1;
 export const weakMap = new WeakMap();
 
 export function queryAPI(obj) {
   if (weakMap.has(obj)) {
-    if (num >= 5) throw new Error('Endpoint load is high');
+    if (num > 3) throw new Error('Endpoint load is high');
     weakMap.set(obj, (num += 1));
   } else {
     num = 1;
