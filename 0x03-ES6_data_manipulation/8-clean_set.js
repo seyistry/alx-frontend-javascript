3df60812e66599db6b238f7cd46de1491d479042
seyistry <seyistry@gmail.com>
2023-04-27 22:00:15 +0100

    update ex

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (startString.length === 0 || startString === null) return '';
   const word = [];
   for (const item of set) {
-    if (item.startsWith(startString)) {
-      word.push(item.slice(startString.length));
+    if (item) {
+      if (item.startsWith(startString)) {
+        word.push(item.slice(startString.length));
+      }
     }
   }
   return word.join('-');
