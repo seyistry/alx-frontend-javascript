e97daa53a4dd63be92ffba9ab0ffe9fb495c63d5
seyistry <seyistry@gmail.com>
2023-04-27 22:29:59 +0100

    change arg type to undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === undefined) return '';
+  if (startString === undefined) return '';
+  if (startString.length === 0) return '';
   const word = [];
   for (const item of set) {
     if (item) {
