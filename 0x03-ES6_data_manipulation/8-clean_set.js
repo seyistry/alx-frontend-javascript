39ef559b7f188e16fe70314b40cbfd8b772698ff
seyistry <seyistry@gmail.com>
2023-04-27 22:02:24 +0100

    change type to undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === null) return '';
+  if (startString.length === 0 || startString === undefined) return '';
   const word = [];
   for (const item of set) {
     if (item) {
